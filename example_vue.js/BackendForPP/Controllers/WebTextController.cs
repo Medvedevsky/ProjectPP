f1d968f8e73f9b0aeea355cb5e7021a5b60688be
stillProger <killumexxx@gmail.com>
2021-05-08 18:22:27 +0400

    Complete v1.0

---
+++ example_vue.js/BackendForPP/Controllers/WebTextController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BackendForPP.Services;
+
+using BackendForPP.Models;
+
+
+namespace BackendForPP.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class WebTextController : ControllerBase
+    {
+        private WebService _service;
+        public WebTextController(WebService service)
+        {
+            _service = service;
+        }
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_service.GetData());
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody]WebText text)
+        {
+            return Ok(_service.PostText(text));
+        }
+    }
+}
